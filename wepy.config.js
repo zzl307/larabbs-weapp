6802981922d9dd3d995c612b8d8118b30f9dac2b
zb <qq1013446945@126.com>
2018-05-24 18:37:11 +0000

    web

--- wepy.config.js
+++ wepy.config.js
@@ -41,6 +41,13 @@ module.exports = {
     }
   },
   plugins: {
+    replace: {
+      filter: /\.js$/,
+      config: {
+        find: /__BASE_URL__/g,
+        replace: prod ? "'https://weapp.larabbs-api/api'" : "'http://larabbs-api.test/api'"
+      }
+    }
   },
   appConfig: {
     noPromiseAPI: ['createSelectorQuery']
@@ -68,6 +75,13 @@ if (prod) {
         png: {
           quality: 80
         }
+      }
+    },
+    replace: {
+      filter: /\.js$/,
+      config: {
+        find: /__BASE_URL__/g,
+        replace: prod ? "'https://weapp.larabbs-api/api'" : "'http://larabbs-api.test/api'"
       }
     }
   }
