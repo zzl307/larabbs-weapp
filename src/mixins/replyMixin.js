4e0039956da03f739f2f7ad004d68108ea4229c9
zb <qq1013446945@126.com>
2018-05-24 18:04:03 +0000

    web

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -68,7 +68,8 @@ export default class ReplyMixin extends wepy.mixin {
       return false
     }
 
-    return (reply.user_id === user.id)
+    // 用户未回复发布者 或 有管理内容权限
+    return (reply.user_id === user.id) || this.$parent.can('manage_contents')
   }
   async onPullDownRefresh() {
     this.noMoreData = false
